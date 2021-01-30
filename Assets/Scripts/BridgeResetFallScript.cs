1b9240fa894bd51898c81cd492303092a763878b
Stefan <stefan_djordjevic1991@hotmail.com>
2021-01-30 20:30:32 +0100

    Added audio to some features

--- Assets/Scripts/BridgeResetFallScript.cs
+++ Assets/Scripts/BridgeResetFallScript.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 public class BridgeResetFallScript : MonoBehaviour
 {
     public GameObject resetPoint;
+
+    public AudioSource splashAudioSource;
+    public AudioSource screamAudioSource;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            splashAudioSource.Play();
+            screamAudioSource.Play();
+            
             other.gameObject.SetActive(false);
             other.gameObject.transform.position = resetPoint.transform.position;
             other.gameObject.SetActive(true);
